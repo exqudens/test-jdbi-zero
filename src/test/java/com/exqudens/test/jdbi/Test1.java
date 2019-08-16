978181e511bba4289ca0a1c3388594823946740a
Andrew Gorin <exqudens@ip-192-168-1-43.ec2.internal>
2019-08-16 20:39:55 +0300

    step 3

--- src/test/java/com/exqudens/test/jdbi/Test1.java
+++ src/test/java/com/exqudens/test/jdbi/Test1.java
@@ -1,15 +1,21 @@
 package com.exqudens.test.jdbi;
 
+import com.exqudens.test.jdbi.model.TabTest1;
 import com.exqudens.test.jdbi.util.HikariConfigUtils;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.BeanMapper;
+import org.jdbi.v3.core.mapper.reflect.FieldMapper;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Test1 {
 
@@ -20,6 +26,7 @@ public class Test1 {
         LOGGER.trace("");
     }
 
+    @Ignore
     @Test
     public void test1() throws Throwable {
         LOGGER.trace("");
@@ -60,6 +67,36 @@ public class Test1 {
                 System.out.println("---");
                 rows = handle.createQuery("select * from `db_test_1`.`tab_test_1`").mapToMap().list();
                 rows.stream().forEach(System.out::println);
+                System.out.println("---");
+            });
+        }
+    }
+
+    @Ignore
+    @Test
+    public void test2() throws Throwable {
+        LOGGER.trace("");
+        HikariConfig hikariConfig = HikariConfigUtils.hikariConfig();
+        try (HikariDataSource dataSource = new HikariDataSource(hikariConfig)) {
+            Jdbi jdbi = Jdbi.create(dataSource);
+            jdbi.useHandle(handle -> {
+                handle.execute("create database if not exists `db_test_1`");
+                handle.execute("create table if not exists `db_test_1`.`tab_test_1`(`id` bigint(20) unsigned not null auto_increment, `name` varchar(255), primary key(`id`))");
+                System.out.println("---");
+                List<Map<String, Object>> rows = handle.createQuery("show databases").mapToMap().list();
+                rows.stream().forEach(System.out::println);
+                System.out.println("---");
+                rows = handle.createQuery("show tables from `db_test_1`").mapToMap().list();
+                rows.stream().forEach(System.out::println);
+                System.out.println("---");
+                handle.createUpdate("truncate table `db_test_1`.`tab_test_1`").execute();
+                System.out.println("---");
+                rows = handle.prepareBatch("insert into `db_test_1`.`tab_test_1`(`name`) values(:superName)").bindBean(new TabTest1(null, "aaa")).add().bindBean(new TabTest1(null, "bbb")).add().executeAndReturnGeneratedKeys().mapToMap().list();
+                rows.stream().forEach(System.out::println);
+                System.out.println("---");
+                handle.registerRowMapper(FieldMapper.factory(TabTest1.class));
+                List<TabTest1> beans = handle.createQuery("select * from `db_test_1`.`tab_test_1`").mapTo(TabTest1.class).list();
+                beans.stream().forEach(System.out::println);
                 System.out.println("---");
             });
         }
